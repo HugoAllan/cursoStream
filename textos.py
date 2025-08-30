fbbab30f6acc0b28833e8830bc52b5d83498cf70
alanhugo <martinezalan0812@gmail.com>
2025-08-30 09:13:15 -0600

    Iniciando

---
+++ textos.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Curso de Streamlit",
+    layout="wide"
+)
+
+st.title("Curso de Streamlit")
+st.header("Soy un header")
+st.subheader("Soy un subheader")
+st.caption("Soy una caption")
+st.text("Soy un texto")
+st.markdown("**Soy un markdown**")
+st.code("print('Hola mundo')")
